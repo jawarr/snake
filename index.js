8ebf0507bb41db247bc2bc5eaf6a6034559f1bca
Jacob <your_email@real_email.edu>
2022-03-01 10:53:18 -0600

    test vscode errors

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 // Game Objects
+let snake = {
+    body: [],
+    nextDirection: []
+  }
 
+let gameState = {
+    apple: [],
+    snake: snake
+}
 
 
 
@@ -9,9 +17,20 @@
 
 
 // Stuff that shows up on the screen
+const table = document.getElementsByTagName('table')[0]
 
+function makeGrid(){
+    for (let i = 0; i < 21; i++){
+        const row = document.createElement('tr')
+        table.appendChild(row)
+        for (let i = 0; i < 21; i++){
+            const cell = document.createElement('td')
+            row.appendChild(cell)
+        }
+    }
+}
 
-
+table.addEventListener('click', makeGrid)
 
 // Make things do stuff when the user does things
 
