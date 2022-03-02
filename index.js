f4540f1bf479bf9fa00286cea0f77a0a4c46c610
Jacob <your_email@real_email.edu>
2022-03-02 09:12:09 -0600

    switch to canvas

--- index.js
+++ index.js
@@ -1,54 +1,38 @@
-// Game Objects
-let snake = {
-    body: [],
-    nextDirection: []
-  }
+const canvas = document.getElementById('board')
+const ctx = canvas.getContext('2d')
 
-let gameState = {
-    score: 0,
-    apple: [],
-    snake: snake
+let speed = 7
+
+function updateLoop () {
+    console.log("I updated")
+    setTimeout(updateLoop, 1000/speed)
 }
 
+// function clearScreen () {
+//     ctx.fillStyle = 'lightgrey'
+//     ctx.fillRect(0, 0, canvas.width, canvas.height)
+// }
+
+// clearScreen()
 
 
-// Functions to manipulate Game Objects
-
-
-
-
-// Stuff that shows up on the screen
-
-// game grid
-const table = document.getElementsByTagName('table')[0]
-
-function makeGrid(){
-    for (let i = 0; i < 5; i++){
-        const row = document.createElement('tr')
-        table.appendChild(row)
-        for (let i = 0; i < 5; i++){
-            const cell = document.createElement('td')
-            row.appendChild(cell)
-        }
-    }
-}
-
-makeGrid()
-
-
-// apple 
-
-const cells = document.getElementsByTagName('td')
-console.log(cells)
-
-setInterval(function () {
-    const randomCellIndex = Math.floor(Math.random() * cells.length);
-    cells[randomCellIndex].classList = ('apple');
-  }, 10000);
+updateLoop()
 
 
 
-  // Make things do stuff when the user does things
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
